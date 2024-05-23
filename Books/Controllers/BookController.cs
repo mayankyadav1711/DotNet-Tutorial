7c2cbec5a64974f2fad28e2fee8ed90d1c5b0379
mayankyadav1711 <mykyadav2003@gmail.com>
2024-05-23 14:09:40 +0530

    Add project files.

---
+++ Books/Controllers/BookController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Books.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        static List <Book> books = new List<Book> ();
+        // GET: api/<BookController>
+        [HttpGet]
+        public IEnumerable<Book> Get()
+        {
+            return books;
+        }
+
+        // GET api/<BookController>/5
+        [HttpGet("{id}")]
+        public Book Get(int id)
+        {
+            return books.FirstOrDefault(s  => s.Id == id);
+        }
+
+        // POST api/<BookController>
+        [HttpPost]
+        public void Post([FromBody] Book value)
+        {
+            books.Add(value);
+        }
+
+        // PUT api/<BookController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Book value)
+        {
+            int i = books.FindIndex(s => s.Id == id);
+            if (i > 0)
+                books[i] = value;
+        }
+
+        // DELETE api/<BookController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            books.RemoveAll(s => s.Id == id);
+        }
+    }
+}
